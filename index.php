b14a65aa7377ff9e7ea6034fcb84c82539db779e
kamalpraemkumar <kamal.praem_kumar@hotmail.com>
2017-02-14 20:46:50 +0800

    CSS changes

--- index.php
+++ index.php
@@ -34,7 +34,19 @@ else {
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>That Big Idea</title>
+      <title>Valentine Wishes From Kamal</title>
+
+      <!-- G Tracking -->
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-84054106-2', 'auto');
+        ga('send', 'pageview');
+
+      </script>
 
       <!-- CSS Dependencies -->
       <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
